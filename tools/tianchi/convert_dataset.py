8e5bcc3f191f2092d83a1fd36f5c48b76569295c
lhexy <xyhe14@gmail.com>
2020-08-06 10:55:09 +0800

    update convert data script

--- tools/tianchi/convert_dataset.py
+++ tools/tianchi/convert_dataset.py
@@ -3,7 +3,7 @@ import glob
 import os
 import os.path as osp
 import SimpleITK as sitk
-from collections import defaultdict
+from collections import defaultdict, Counter
 
 import mmcv
 import numpy as np
@@ -21,85 +21,123 @@ OFFICIAL_ANNS = {
     'lumbar_train51': 'annotations/lumbar_train51_annotation.json',
 }
 
+META_KEYS = {
+    'study_uid': '0020|000d',
+    'series_uid': '0020|000e',
+    'instance_uid': '0008|0018',
+    'series_description': '0008|103e',
+    'image_position': '0020|0032',
+    'image_orientation': '0020|0037',
+    'slice_thickness': '0018|0050',
+    'pixel_spacing': '0028|0030',
+}
 
-class Study(object):
-    META_KEYS = {
-        'study_uid': '0020|000d',
-        'series_uid': '0020|000e',
-        'instance_uid': '0008|0018',
-        'series_description': '0008|103e',
-        'image_position': '0020|0032',
-        'image_orientation': '0020|0037',
-        'slice_thickness': '0018|0050',
-        'pixel_spacing': '0028|0030',
-    }
 
-    def __init__(self, study_id, study_path):
-        self._study_id = study_id
-        # study_idx is the dirname of study in official dataset
-        self._study_idx = osp.basename(study_path)
+class Series:
+    """Series object for handling series data info
+    and some processing methods.
 
-        # get all DICOM files in the study dir
-        self._dicom_filenames = glob.glob(osp.join(study_path, '*.dcm'))
+    Args:
+        series_uid (str): series Uid
+        dicoms (list[dict]): a list of dicom info dicts
+    """
+
+    def __init__(self, series_uid, dicoms):
+        self._series_uid = series_uid
+        self._dicoms = self._filter_dicoms(dicoms)
+
+        # set the plane of the series
+        planes = Counter([dicom['plane'] for dicom in self._dicoms])
+        self._plane = planes.most_common(1)[0][0]
+
+        # filter with specified plane
+        self._dicoms = [
+            dicom for dicom in self._dicoms if dicom['plane'] == self._plane
+        ]
+        # series description
+        for dicom in self._dicoms:
+            if 'series_description' in dicom:
+                _description = dicom['series_description']
+                break
+            else:
+                _description = 'none'
+        self._description = _description
+
+        # positions
+        positions = np.stack(
+            [dicom['image_position'] for dicom in self._dicoms])
+        if self._plane == 'sagittal':
+            dim = 0  # x
+        elif self._plane == 'coronal':
+            dim = 1  # y
+        elif self._plane == 'transverse':
+            dim = 2  # z
+        else:
+            raise ValueError(f'Get wrong plane {self._plane}.')
+        inds = np.argsort(positions[:, dim])
+        self._positions = positions[inds]
+        # sort dicoms with positions
+        self._dicoms = [self._dicoms[idx] for idx in inds]
 
     @property
-    def study_id(self):
-        return self._study_id
+    def series_uid(self):
+        return self._series_uid
 
     @property
-    def study_idx(self):
-        return self._study_idx
+    def plane(self):
+        return self._plane
 
     @property
-    def study_uid(self):
-        assert hasattr(self, 'dicoms'), 'Please run load_all_dicoms first'
-        return self.dicoms[-1]['study_uid']
+    def series_description(self):
+        return self._description
 
-    def info(self):
-        _info = dict(
-            id=self._study_id,
-            study_idx=self._study_idx,
-            study_uid=self.study_uid)
+    @property
+    def positions(self):
+        return self._positions
 
-        return _info
+    def _filter_dicoms(self, dicoms):
+        valids = []
+        for i, dicom in enumerate(dicoms):
+            if 'image_position' not in dicom:
+                continue
+            if 'plane' not in dicom:
+                continue
+            valids.append(dicom)
 
-    def _convert_to_img(self, reader, img_path):
-        """Convert DICOM to JPEG image.
+        return valids
 
-        Args:
-            reader (sitk.Reader): DICOM file reader.
-            img_path (str): Path to store converted image.
+    def anns(self, series_id, study_id, study_idx, study_uid):
+        """Generate annotation for series."""
+        data = dict(
+            id=series_id,
+            series_uid=self._series_uid,
+            plane=self._plane,
+            series_description=self._description,
+            # study info
+            study_id=study_id,
+            study_idx=study_idx,
+            study_uid=study_uid,
+            # dicom info
+            instance_uids=[dicom['instance_uid'] for dicom in self._dicoms])
 
-        Returns:
-            img (np.ndarray): Converted image.
-        """
-        img = reader.Execute()
-        img = sitk.GetArrayFromImage(img)[0]
-        img = img.astype(np.float32)
-        # output_pixel = (input_pixel - input_min) * \
-        #   ((output_max - output_min) / (input_max - input_min)) + \
-        #   output_min
-        img = (img - img.min()) * (255.0 / (img.max() - img.min()))
-        if reader.GetMetaData('0028|0004').strip() == 'MONOCHROME1':
-            img = 255.0 - img
+        return data
 
-        img = np.ascontiguousarray(img)
-        mmcv.imwrite(img, img_path)
 
-        return img
+class Study:
 
-    def _parse_meta(self, meta):
+    @staticmethod
+    def _parse_meta(meta):
         _meta = {}
-        for keyname, meta_key in self.META_KEYS.items():
+        for keyname, meta_key in META_KEYS.items():
             if meta_key not in meta:
                 continue
             _meta[keyname] = meta[meta_key]
 
-        process_items = [
+        processing_items = [
             'image_position', 'image_orientation', 'slice_thickness',
             'pixel_spacing'
         ]
-        for key in process_items:
+        for key in processing_items:
             if key not in _meta:
                 continue
             _meta[key] = list(map(float, _meta[key].strip().split('\\')))
@@ -109,9 +147,9 @@ class Study(object):
             rd = np.array(_meta['image_orientation'][:3], dtype=np.float32)
             cd = np.array(_meta['image_orientation'][3:], dtype=np.float32)
             normal = np.cross(rd, cd)
-            normal = normal / np.linalg.norm(normal)
+            normal = np.abs(normal / np.linalg.norm(normal))
 
-            thr = 0.86
+            thr = 0.8660254037844387  # pi / 6
             xd = np.array([1, 0, 0], dtype=np.float32)
             yd = np.array([0, 1, 0], dtype=np.float32)
             zd = np.array([0, 0, 1], dtype=np.float32)
@@ -128,24 +166,16 @@ class Study(object):
 
         return _meta
 
-    def load_all_dicoms(self, reader, dicom_id, img_prefix):
-        """Get all DICOM files of the study instance.
-
-        Args:
-            reader (sitk.Reader): SimpleITK DICOM file reader.
-            dicom_id (int): Global dicom_id, starting from 1.
-            img_prefix (str): Path to converted images.
-
-        Returns:
-            dicoms_list (list[dict]): A list of each dicom avaiable meta info.
-            dicom_id (int): increased dicom_id.
-        """
-        # imgs will be stored in ``{img_prefix}/{study_idx}``
-        img_prefix = osp.join(img_prefix, self._study_idx)
+    @staticmethod
+    def _load_all_dicoms(study_id, study_idx, dicom_filenames):
+        # set DICOM file reader
+        reader = sitk.ImageFileReader()
+        reader.LoadPrivateTagsOn()
+        reader.SetImageIO('GDCMImageIO')
 
         dicoms_list = []
-        for dicom_filename in self._dicom_filenames:
-            # read DICOM file
+        for dicom_filename in dicom_filenames:
+            # TODO: multi-processes
             reader.SetFileName(dicom_filename)
             try:
                 reader.ReadImageInformation()
@@ -153,35 +183,144 @@ class Study(object):
                 # some DICOM files are unreadable
                 continue
 
-            _dicom_dict = dict(id=dicom_id, study_id=self._study_id)
-
-            filename = osp.basename(dicom_filename)
-            filename = filename.replace('.dcm', '.jpg')
-            _dicom_dict['filename'] = filename
-
+            _dicom_dict = dict(
+                study_id=study_id,
+                study_idx=study_idx,
+                filename=osp.basename(dicom_filename))
             meta = {
                 key: reader.GetMetaData(key)
                 for key in reader.GetMetaDataKeys()
             }
-            _dicom_dict.update(self._parse_meta(meta))
+            _dicom_dict.update(Study._parse_meta(meta))
+            dicoms_list.append(_dicom_dict)
 
-            # image height and width
+        return dicoms_list
+
+    @staticmethod
+    def fromfiles(study_id, path):
+        """
+        Args:
+            study_id (int): study_id
+            path (str): path of study dir, e.g., "somepath/stduy*/"
+
+        Returns:
+            Study
+        """
+        study_idx = osp.basename(path)
+
+        # get all DICOM files in the study dir (path)
+        dicom_filenames = glob.glob(osp.join(path, '*.dcm'))
+        # dicoms_list: list[dict]
+        dicoms_list = Study._load_all_dicoms(study_id, study_idx,
+                                             dicom_filenames)
+
+        return Study(study_id, study_idx, dicoms_list, path)
+
+    def __init__(self, study_id, study_idx, dicoms, dicom_prefix=None):
+        """
+        Args:
+            study_id (int): study id
+            study_idx (str): "study*"
+            dicoms (list[dict]): a list of dicom info dicts
+            dicom_prefix(str, optional): prefix of the DICOM files
+        """
+        self._study_id = study_id
+        self._study_idx = study_idx
+        self._dicom_prefix = dicom_prefix
+        self.dicoms = dicoms
+
+        # for handling more fields
+        self._fields = {}
+
+        # series
+        series_dict = defaultdict(list)
+        for dicom in self.dicoms:
+            series_dict[dicom['series_uid']].append(dicom)
+        self.series_list = []
+        self.series_planes = []
+        self.series_descriptions = []
+        for series_uid, _dicoms_list in series_dict.items():
+            series = Series(series_uid, _dicoms_list)
+            self.series_list.append(series)
+            self.series_planes.append(series.plane)
+            self.series_descriptions.append(series.series_description)
+        self.num_series = len(self.series_list)
+
+    @property
+    def study_id(self):
+        return self._study_id
+
+    @property
+    def study_idx(self):
+        return self._study_idx
+
+    @property
+    def study_uid(self):
+        assert hasattr(self, 'dicoms')
+        return self.dicoms[-1]['study_uid']
+
+    def _convert_to_jpg(self, dicom_path, img_path):
+        reader = sitk.ImageFileReader()
+        reader.LoadPrivateTagsOn()
+        reader.SetImageIO('GDCMImageIO')
+        img = reader.Execute()
+
+        img = sitk.GetArrayFromImage(img)[0]
+        img = img.astype(np.float32)
+        # output_pixel = (input_pixel - input_min) * \
+        #   ((output_max - output_min) / (input_max - input_min)) + \
+        #   output_min
+        img = (img - img.min()) * (255.0 / (img.max() - img.min()))
+        if reader.GetMetaData('0028|0004').strip() == 'MONOCHROME1':
+            img = 255.0 - img
+
+        img = np.ascontiguousarray(img)
+        mmcv.imwrite(img, img_path)
+
+        return img
+
+    def read_or_convert_images(self, img_prefix):
+        """
+        Read images of study, or convert to JPG images if not exists.
+
+        Args:
+            img_prefix (str): path of stored JPG images
+        """
+        # imgs will be stored in ``{img_prefix}/{study_idx}``
+        img_prefix = osp.join(img_prefix, self._study_idx)
+
+        for dicom in self.dicoms:
+            filename = dicom['filename']
+            filename = filename.replace('.dcm', '.jpg')
+
             img_path = osp.join(img_prefix, filename)
             if osp.exists(img_path):
                 img = mmcv.imread(img_path)
             else:
-                # no JPEG image, convert DICOM to JPEG
-                img = self._convert_to_img(reader, img_path)
+                # no JPG image, convert DICOM to JPG
+                img = self._convert_to_jpg(
+                    osp.join(self._dicom_prefix, dicom['filename']), img_path)
             height, width = img.shape[:2]
-            _dicom_dict['height'] = height
-            _dicom_dict['width'] = width
+            # update dicom info
+            dicom['filename'] = filename
+            dicom['height'] = height
+            dicom['width'] = width
 
-            dicoms_list.append(_dicom_dict)
-            dicom_id += 1
+    def update(self, key, value):
+        self._fields[key] = value
 
-        # set Study attributes
-        self.dicoms = dicoms_list
-        return dicoms_list, dicom_id
+    def anns(self):
+        """Generate annotation for study."""
+        data = dict(
+            id=self._study_id,
+            study_idx=self._study_idx,
+            study_uid=self.study_uid)
+
+        for key in ['series_uid', 'instance_uid']:
+            if key in self._fields:
+                data[key] = self._fields[key]
+
+        return data
 
 
 def get_cat_id(tag, code):
@@ -343,34 +482,51 @@ def main():
     studies = glob.glob(osp.join(dicom_prefix, 'study*'))
     print(f'There are {len(studies)} study instances in {dicom_prefix}')
 
-    # Step. 2: set DICOM file reader
-    reader = sitk.ImageFileReader()
-    reader.LoadPrivateTagsOn()
-    reader.SetImageIO('GDCMImageIO')
-
-    # Step. 3: get all DICOMs for each study instance
-    dicom_id = 1  # starts from 1, global dicom id
-    dicoms_list = []  # all dicoms will be stored in the list
-    # keep a dict of studies (using study_uid as key) for
-    # adding official annotations in the following
+    # Step. 2: get all DICOMs for each study instance
     studies_dict = defaultdict(dict)
     for i, study in enumerate(studies):
         study_id = i + 1  # study_id starts from 1
-        study = Study(study_id, study)
-        _dicoms_list, dicom_id = study.load_all_dicoms(reader, dicom_id,
-                                                       img_prefix)
-        dicoms_list.extend(_dicoms_list)
-        studies_dict[study.study_uid] = study.info()
+        # init Study instance
+        study = Study.fromfiles(study_id, study)
+        # read or convert images, update info about images
+        study.read_or_convert_images(img_prefix)
+        studies_dict[study.study_uid] = study
 
-    # Step. 4: set all categories
+    # Step. 3: series
+    series_id = 1
+    series_list = []
+    for study_uid, study in studies_dict.items():
+        for _series in study.series_list:
+            _series_anns = _series.anns(
+                series_id=series_id,
+                study_id=study.study_id,
+                study_idx=study.study_idx,
+                study_uid=study.study_uid)
+
+            series_list.append(_series_anns)
+            series_id += 1
+
+    # Step. 4: dicoms
+    dicom_id = 1
+    dicoms_list = []
+    for _, study in studies_dict.items():
+        for dicom in study.dicoms:
+            dicom['id'] = dicom_id
+            dicoms_list.append(dicom)
+            dicom_id += 1
+
+    # Step. 5: set all categories
     categories = []
     for i, category in enumerate(CLASSES):
+        # ('disc: v1', 'disc: v2', 'disc: v3', 'disc: v4', 'disc: v5',
+        #  'vertebra: v1', 'vertebra: v2')
         cat = dict(id=i + 1)  # cat_id starts from 1
+        cat['name'] = category
         cat['tag'] = category.split(':')[0].strip()  # disc, vertebra
         cat['code'] = category.split(':')[1].strip()  # v1, v2, v3, v4, v5
         categories.append(cat)
 
-    # Step. 5 (optional): load official annotations
+    # Step. 6 (optional): load official annotations
     if with_anns:
         print(f'Loading offiicial annotation file {official_ann}')
         ori_anns = mmcv.load(official_ann)
@@ -381,27 +537,28 @@ def main():
             # study info in official annotion file, e.g.,
             # seriesUid, instanceUid
             study_uid = _ann['studyUid']
-            studies_dict[study_uid]['series_uid'] = _ann['data'][0][
-                'seriesUid']
-            studies_dict[study_uid]['instance_uid'] = _ann['data'][0][
-                'instanceUid']
+            studies_dict[study_uid].update('series_uid',
+                                           _ann['data'][0]['seriesUid'])
+            studies_dict[study_uid].update('instance_uid',
+                                           _ann['data'][0]['instanceUid'])
 
-            study_id = studies_dict[study_uid]['id']
+            study_id = studies_dict[study_uid].study_id
             points = _ann['data'][0]['annotation'][0]['data']['point']
             _anns_list, ann_id = load_official_anns(points, study_id, ann_id)
             anns_list.extend(_anns_list)
 
-    # Step. 6: output some info
+    # Step. 7: output some info
     print(f'There are {len(dicoms_list)} DICOMs in total.')
+    print(f'There are {len(series_list)} series in total.')
     print(f'There are {len(categories)} categories in total. They are: ')
     for cat in categories:
         print(f'  {cat}')
     if with_anns:
         print(f'There are {len(anns_list)} converted annotations in total.')
 
-    # Step. 7: write ann_file
+    # Step. 8: write ann_file
     # convert studies_dict to studies_list
-    studies_list = [study for _, study in studies_dict.items()]
+    studies_list = [study.anns() for _, study in studies_dict.items()]
     anns = dict(
         studies=studies_list, dicoms=dicoms_list, categories=categories)
     if with_anns:
